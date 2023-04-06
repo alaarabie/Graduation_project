9f55976843fb25c42d42828f3f784fbee74f2187
ali <ali.alimaher27@gmail.com>
2023-04-06 10:55:13 +0200

    rename

--- tb/seq_lib/base_seq.sv
+++ tb/seq_lib/base_seq.sv
@@ -1,6 +1,6 @@
-class rf_base_seq extends  uvm_sequence #(uvm_sequence_item);
+class base_seq extends  uvm_sequence #(uvm_sequence_item);
 
-  `uvm_object_utils(rf_base_seq)
+  `uvm_object_utils(base_seq)
 
   rf_reg_block rf_rb;
 
@@ -13,14 +13,14 @@ class rf_base_seq extends  uvm_sequence #(uvm_sequence_item);
   extern function new (string name = "");
   extern task body();
 
-endclass : rf_base_seq
+endclass : base_seq
 
 
-function rf_base_seq::new(string name = "");
+function base_seq::new(string name = "");
   super.new(name);
 endfunction : new
 
-task rf_base_seq::body();
+task base_seq::body();
   if(m_cfg == null) begin
     `uvm_fatal(get_full_name(), "env_config is null")
   end
