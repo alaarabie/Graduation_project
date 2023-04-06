e148be316de1914ea85bb0a60242b6b917f37728
ali <ali.alimaher27@gmail.com>
2023-04-06 10:50:27 +0200

    wrong inputs/outputs declaration

--- tb/register_file_agent/rf_if.sv
+++ tb/register_file_agent/rf_if.sv
@@ -4,14 +4,14 @@ interface rf_if#(HMC_RF_WWIDTH = 64,
                  (
                   input clk, 
                   input res_n,
-                  output logic  [HMC_RF_AWIDTH-1:0] rf_address,
-                  output logic  [HMC_RF_WWIDTH-1:0] rf_write_data,
-                  input  logic  [HMC_RF_RWIDTH-1:0] rf_read_data,
-                  input  logic                      rf_access_complete,
-                  input  logic                      rf_invalid_address,
-                  output logic                      rf_read_enable,
-                  output logic                      rf_write_enable
                   );
 
+logic  [HMC_RF_AWIDTH-1:0] rf_address;         //output
+logic  [HMC_RF_WWIDTH-1:0] rf_write_data;      //output
+logic  [HMC_RF_RWIDTH-1:0] rf_read_data;       //input
+logic                      rf_access_complete; //input
+logic                      rf_invalid_address; //input
+logic                      rf_read_enable;     //output
+logic                      rf_write_enable;    //output
 
 endinterface : rf_if
