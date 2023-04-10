71a49b0d5b7c592f992b5344b0be15ad14af5fb6
ali <ali.alimaher27@gmail.com>
2023-04-10 20:41:59 +0200

    may be useful if we want to read from a certain register for a period of time

---
+++ tb/seq_lib/rf_status_init_mirror_seq.sv
@@ -0,0 +1,21 @@
+class rf_status_init_mirror_seq extends  base_seq;
+
+  `uvm_object_utils(rf_status_init_mirror_seq)
+
+  extern function new (string name = "");
+  extern task body();
+
+endclass : rf_status_init_mirror_seq
+
+
+function rf_status_init_mirror_seq::new(string name = "");
+  super.new(name);
+endfunction : new
+
+task rf_status_init_mirror_seq::body();
+  super.body();
+
+  // will continuously perform read operations to mirror the hardware into the register model
+  rf_rb.m_reg_status_init.mirror(status, .path(UVM_FRONTDOOR), .parent(this));
+
+endtask : body
