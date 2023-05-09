bc80dafbc480db7e166e878033036da91edc5626
NadaOthman20 <129794938+NadaOthman20@users.noreply.github.com>
2023-05-09 10:22:58 +0300

    Update axi_pkg.sv

--- tb/axi/axi_pkg.sv
+++ tb/axi/axi_pkg.sv
@@ -1,18 +1,17 @@
 `include "axi_interface.sv"
- 
-
 
 package axi_pkg;
 `include "uvm_macros.svh"
 import uvm_pkg::*;
+import cmd_pkg::*;
 
-`include "axi_monitor.sv"
-`include "valid_data.sv"
-`include "axi_sequencer.sv"
-`include "axi_monitor.sv"
-`include "axi_env.sv"
-`include "axi_driver.sv"
 `include "axi_config.sv"
+`include "axi_env.sv"
 `include "axi_agent.sv" 
+`include "axi_driver.sv"
+`include "axi_monitor.sv"
+`include "axi_sequencer.sv"
+`include "valid_data.sv"
+`include "hmc_pkt_item_request.sv"
 
 endpackage : axi_pkg
