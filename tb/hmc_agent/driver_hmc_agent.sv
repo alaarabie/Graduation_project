1b589f2ba8f245a07ec442072a355424f87a453b
mohamed16hosam <mohamed.yassen99@eng-st.cu.edu.eg>
2023-05-10 03:37:18 +0300

    jaaaa

--- tb/hmc_agent/driver_hmc_agent.sv
+++ tb/hmc_agent/driver_hmc_agent.sv
@@ -37,7 +37,15 @@ class driver_hmc_agent #(DWIDTH = 512 ,
         
         forever begin : response_loop
         
-          wait(vif.res_n)
+          if(!(vif.res_n))
+            begin
+             vif.LXTXPS=1'b0 ;
+             vif.FERR_N=1'b1 ;
+             vif.phy_data_rx_phy2link='b0 ;
+             vif.phy_rx_ready=1'b0 ;
+             vif.phy_tx_ready=1'b0 ;                 
+            end
+      
 
         // rf_request_item state_item ;
 
@@ -49,88 +57,95 @@ class driver_hmc_agent #(DWIDTH = 512 ,
         // hmc_agent_if.send_rf(state_item,rf_read_data) ; // rf_read_data is an output of task send_rf in the if
         // tx_init_state = rf_read_data[HMC_RF_WWIDTH-11:HMC_RF_WWIDTH-12] ;
         // seq_item_port.item_done() ;
+          else
+           begin
+            vif.LXTXPS=1'b1 ;  
+            vif.FERR_N=1'b1 ;  
+            vif.phy_rx_ready=1'b1 ;   
+            vif.phy_tx_ready=1'b1 ;                            
 
-     current_request_packet.delete() ;  
-     vif.vif_request_packet.delete() ;             
+            current_request_packet.delete() ;  
+            vif.vif_request_packet.delete() ;             
      
-//for state sequence
-request_packet=hmc_pkt_item::type_id::create("request_packet") ;
-response_packet=hmc_pkt_item::type_id::create("response_packet") ;
+            //for state sequence
+            request_packet=hmc_pkt_item::type_id::create("request_packet") ;
+            response_packet=hmc_pkt_item::type_id::create("response_packet") ;
 
 
-      seq_item_port.get_next_item(response_packet);
-      
-      if (vif.phy_data_tx_link2phy[FLIT_SIZE-1:0]!='b0) begin
-         response_packet.new_request=1'b1 ;
-         vif.k=1 ;          
-      end 
-      else begin
-        response_packet.new_request=1'b0; 
-      end 
+                  seq_item_port.get_next_item(response_packet);
+                  
+                  if (vif.phy_data_tx_link2phy[FLIT_SIZE-1:0]!='b0) begin
+                     response_packet.new_request=1'b1 ;
+                     vif.k=1 ;          
+                  end 
+                  else begin
+                    response_packet.new_request=1'b0; 
+                  end 
 
-      packing_FLITS() ;
-      `uvm_info("hmc_vseq", $sformatf("current_request_packet=%p",current_request_packet) ,UVM_LOW)      
-      vif.vif_request_packet=current_request_packet ;  
-      // {<<bit{vif.vif_request_packet}}=current_request_packet ;      
-      vif.z=1 ;
+                  packing_FLITS() ;
+                  `uvm_info("hmc_vseq", $sformatf("current_request_packet=%p",current_request_packet) ,UVM_LOW)      
+                  vif.vif_request_packet=current_request_packet ;  
+                  // {<<bit{vif.vif_request_packet}}=current_request_packet ;      
+                  vif.z=1 ;
 
-      seq_item_port.item_done() ; 
+                  seq_item_port.item_done() ; 
 
-      if((response_packet.init_state!=2'b11)&&(response_packet.init_state!=2'b01))
-      begin
-//hmc_initialization sequence(NULL) 
-        seq_item_port.get_next_item(request_packet) ;      
-        request_packet.init_state=response_packet.init_state ;
-        seq_item_port.item_done() ; 
+                  if((response_packet.init_state!=2'b11)&&(response_packet.init_state!=2'b01))
+                  begin
+            //hmc_initialization sequence(NULL) 
+                    seq_item_port.get_next_item(request_packet) ;      
+                    request_packet.init_state=response_packet.init_state ;
+                    seq_item_port.item_done() ; 
 
-        seq_item_port.get_next_item(response_packet) ;
-        assert (response_packet.pack(current_response_packet)); // call do_pack
-//      response_packet.pack(current_response_packet) ;
-        vif.send_to_DUT(current_response_packet,response_packet) ;  // to send the response packet to the DUT
-        //vif.send_to_DUT(response_packet) ;  // to send the response packet to the DUT
-        seq_item_port.item_done() ; 
-      end  
-      else if (response_packet.init_state==2'b01) begin
-//Training Sequence        
-         for (i = 4'b0; i <= 4'b0111; i++) begin
-            vif.phy_data_rx_phy2link[63:0] = {48'b0,4'b1111,4'b0,4'b0011,i} ;
-            for (int j = 0; j <6 ; j++) begin
-            vif.phy_data_rx_phy2link [127+(j*64) -: 63] = {48'b0,4'b1111,4'b0,4'b0101,i} ;                  
-            //vif.phy_data_rx_phy2link[127+(j*64):64+(j*64)] = {48'b0,4'b1111,4'b0,4'b0101,i} ;               
-            end
-            vif.phy_data_rx_phy2link[511:448] = {48'b0,4'b1111,4'b0,4'b1100,i} ;
-            @(posedge vif.clk) ;
-         end           
-      end
-      //else begin
-//hmc_response sequence 
-        // seq_item_port.get_next_item(request_packet) ;
-        // request_packet.unpack(current_request_packet) ;              
-        // seq_item_port.item_done() ; 
+                    seq_item_port.get_next_item(response_packet) ;
+                    assert (response_packet.pack(current_response_packet)); // call do_pack
+            //      response_packet.pack(current_response_packet) ;
+                    vif.send_to_DUT(current_response_packet,response_packet) ;  // to send the response packet to the DUT
+                    //vif.send_to_DUT(response_packet) ;  // to send the response packet to the DUT
+                    seq_item_port.item_done() ; 
+                  end  
+                  else if (response_packet.init_state==2'b01) begin
+            //Training Sequence        
+                     for (i = 4'b0; i <= 4'b0111; i++) begin
+                        vif.phy_data_rx_phy2link[63:0] = {48'b0,4'b1111,4'b0,4'b0011,i} ;
+                        for (int j = 0; j <6 ; j++) begin
+                        vif.phy_data_rx_phy2link [127+(j*64) -: 63] = {48'b0,4'b1111,4'b0,4'b0101,i} ;                  
+                        //vif.phy_data_rx_phy2link[127+(j*64):64+(j*64)] = {48'b0,4'b1111,4'b0,4'b0101,i} ;               
+                        end
+                        vif.phy_data_rx_phy2link[511:448] = {48'b0,4'b1111,4'b0,4'b1100,i} ;
+                        @(posedge vif.clk) ;
+                     end           
+                  end
+                  //else begin
+            //hmc_response sequence 
+                    // seq_item_port.get_next_item(request_packet) ;
+                    // request_packet.unpack(current_request_packet) ;              
+                    // seq_item_port.item_done() ; 
 
-        // seq_item_port.get_next_item(response_packet) ;
-        // response_packet.pack(current_response_packet) ;        
-        // vif.send_to_DUT(current_response_packet) ;  // to send the response packet to the DUT
-        // seq_item_port.item_done() ; 
-      //end
+                    // seq_item_port.get_next_item(response_packet) ;
+                    // response_packet.pack(current_response_packet) ;        
+                    // vif.send_to_DUT(current_response_packet) ;  // to send the response packet to the DUT
+                    // seq_item_port.item_done() ; 
+                  //end
 
 
-         // assert(tx_init_state==2'b11)
-         //    begin
-         //       TX_FSM() ;
-         //    end
-         // else
-         //    begin
-         //     assert(vif.phy_data_tx_link2phy[FLIT_SIZE-1:0]=='b0)
-         //      begin  
-         //        packing_FLITS(response_packet) ;
-         //        response_packet.unpack(current_request_packet) ;
-         //        seq_item_port.get_next_item(response_packet) ;
-         //        response_packet.pack(current_response_packet) ;
-         //        vif.send_hmc(response_packet) ;  // to send the response packet to the DUT
-         //        seq_item_port.item_done() ;  
-         //     end
-         //    end
+                     // assert(tx_init_state==2'b11)
+                     //    begin
+                     //       TX_FSM() ;
+                     //    end
+                     // else
+                     //    begin
+                     //     assert(vif.phy_data_tx_link2phy[FLIT_SIZE-1:0]=='b0)
+                     //      begin  
+                     //        packing_FLITS(response_packet) ;
+                     //        response_packet.unpack(current_request_packet) ;
+                     //        seq_item_port.get_next_item(response_packet) ;
+                     //        response_packet.pack(current_response_packet) ;
+                     //        vif.send_hmc(response_packet) ;  // to send the response packet to the DUT
+                     //        seq_item_port.item_done() ;  
+                     //     end
+                     //    end
+           end         
         end: response_loop
      endtask : run_phase 
 
