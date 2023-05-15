97d727dd60daf9fe1ed8345da9884be5604c8636
unknown <rahmahamdy107@gmail.com>
2023-05-16 00:40:58 +0300

    fix errors

--- tb/env_cfg.sv
+++ tb/env_cfg.sv
@@ -15,7 +15,8 @@ class env_cfg extends uvm_object;
   // Configurations for the sub_components
   rf_agent_cfg_t m_rf_agent_cfg;
   hmc_agent_config_t m_hmc_agent_cfg;
-
+  axi_config_t  m_axi_cfg;
+  
   //Register block
   rf_reg_block rf_rb;
 
