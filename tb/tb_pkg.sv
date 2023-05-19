abe831c4b32874582bb4ea79704cdebd0cdbe101
unknown <rahmahamdy107@gmail.com>
2023-05-19 16:13:19 +0300

    modified

--- tb/tb_pkg.sv
+++ tb/tb_pkg.sv
@@ -2,11 +2,12 @@ package tb_pkg ;
 	import uvm_pkg::*;
 	`include "uvm_macros.svh"
     import cmd_pkg::*;
+    import axi_pkg::* ;
     import hmc_agent_pkg::* ;
     import rf_reg_block_pkg::* ;    
     import rf_agent_pkg::* ;
     import tb_params_pkg::* ;
-    import axi_pkg::* ;
+
     
 	`include "env_cfg.sv"
 	`include "vsequencer.sv"	
