ad0eeff8192c4da3a71f8b87dcc474ed1490a6c0
AliMaher15 <ali.alimaher27@gmail.com>
2023-06-14 05:22:52 +0300

    connect ports

--- tb/coverage.sv
+++ tb/coverage.sv
@@ -1,70 +1,68 @@
-
-
 class coverage extends uvm_subscriber #(hmc_pkt_item);
  `uvm_component_utils(coverage)
 
+  // Variables
+     bit   [3:0]      length;
+     cmd_encoding_e   command;
 
-     bit   [3:0]      length;              // LNG    [10: 7]
-     cmd_encoding_e   command;             // CMD    [ 5: 0]
-
+  //***********************************************************//
+  //*********************** COVERGROUPS ***********************//
+  //***********************************************************//
      covergroup cmd_cov;
 
       hmc_cmd : coverpoint command {
       bins flow[]={ NULL,
-      PRET,
-      TRET,
-      IRTRY};
-
+                    PRET,
+                    TRET,
+                    IRTRY};
 
       bins Write_Requests[]={ WR16,
-      WR32,
-      WR48,
-      WR64,
-      WR80,
-      WR96,
-      WR112,
-      WR128};
-
+                              WR32,
+                              WR48,
+                              WR64,
+                              WR80,
+                              WR96,
+                              WR112,
+                              WR128};
 
       bins Misc_Write_Requests[]={ MD_WR,
-      BWR,
-      DUAL_2ADD8 ,
-      SINGLE_ADD16};
+                                   BWR,
+                                   DUAL_2ADD8 ,
+                                   SINGLE_ADD16};
 
       bins  Posted_Write_Requests[]={ P_WR16,
-      P_WR32,
-      P_WR48,
-      P_WR64,
-      P_WR80,
-      P_WR96,
-      P_WR112,
-      P_WR128};
+                                      P_WR32,
+                                      P_WR48,
+                                      P_WR64,
+                                      P_WR80,
+                                      P_WR96,
+                                      P_WR112,
+                                      P_WR128};
 
       bins Posted_Misc_Write_Requests[]={ P_BWR,
-      P_DUAL_2ADD8,
-      P_SINGLE_ADD16};
+                                          P_DUAL_2ADD8,
+                                          P_SINGLE_ADD16};
 
       bins  Mode_Read_Request[]={MD_RD};
 
       bins  Read_Requests[]={ RD16,
-      RD32,
-      RD48,
-      RD64,
-      RD80,
-      RD96,
-      RD112,
-      RD128};
+                              RD32,
+                              RD48,
+                              RD64,
+                              RD80,
+                              RD96,
+                              RD112,
+                              RD128};
 
       bins Response_Commands[]={RD_RS,
-      WR_RS,
-      MD_RD_RS,
-      MD_WR_RS,
-      ERROR_RS };
+                                WR_RS,
+                                MD_RD_RS,
+                                MD_WR_RS,
+                                ERROR_RS };
       
       }
 
     endgroup
-    
     
     covergroup pkt_cov;
      
@@ -73,9 +71,9 @@ class coverage extends uvm_subscriber #(hmc_pkt_item);
      
    endgroup
    
-   
-   
-
+  //***********************************************************//
+  //********************** FUNCTIONS **************************//
+  //***********************************************************//
    function new (string name, uvm_component parent);
     super.new(name, parent);
     cmd_cov = new();
@@ -83,11 +81,7 @@ class coverage extends uvm_subscriber #(hmc_pkt_item);
     
   endfunction : new
 
-
-
-
   function void write(hmc_pkt_item t);
-    
     command=t.command;
     length=t.length;
     
@@ -95,9 +89,5 @@ class coverage extends uvm_subscriber #(hmc_pkt_item);
     pkt_cov.sample();
     
   endfunction : write
-  
-  
-  
-  
   
 endclass : coverage
--- tb/env.sv
+++ tb/env.sv
@@ -91,6 +91,12 @@ function void env::connect_phase(uvm_phase phase);
 
   // Connect Coverage and Scoreboard analysis ports
   // Syntax :
-  //m_agent.m_monitor.ap_name.connect(m_scoreboard.ap_name.analysis_export);
+  //m_agent.ap_name.connect(m_scoreboard.ap_name.analysis_export);
+  
+  //axi_agent_h.mon_request.connect(m_coverage.analysis_export);  // Should send the hmc_pkt_item
+  //axi_agent_h.mon_response.connect(m_coverage.analysis_export); // Should send the hmc_pkt_item
+
+  hmc_agent_h.mon_req_ap.connect(m_coverage.analysis_export);
+  hmc_agent_h.mon_res_ap.connect(m_coverage.analysis_export);
 
 endfunction : connect_phase
