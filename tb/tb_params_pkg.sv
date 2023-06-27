facdb42d7e0bdf6d2eebe6cf6ada863422d2111f
AliMaher15 <ali.alimaher27@gmail.com>
2023-06-27 16:12:19 +0300

    parameters edits and new hmc handling

--- tb/tb_params_pkg.sv
+++ tb/tb_params_pkg.sv
@@ -4,59 +4,53 @@ import axi_pkg::*;
 import rf_agent_pkg::*;
 import hmc_agent_pkg::*;
 
-
-
-class HMC;
-
-  //Define width of the datapath
-  localparam FPW = 4;        //Legal Values: 2,4,6,8
-  localparam LOG_FPW               = 2 ;        //Legal Values: 1 for FPW=2 ,2 for FPW=4 ,3 for FPW=6/8
-  localparam DWIDTH                = FPW*128 ;  //Leave untouched
+  parameter FPW                   = 4;        //Legal Values: 2,4,6,8
+  parameter LOG_FPW               = 2;        //Legal Values: 1 for FPW=2 ,2 for FPW=4 ,3 for FPW=6/8
+  parameter DWIDTH                = FPW*128;  //Leave untouched
   //Define HMC interface width
-  localparam LOG_NUM_LANES         = 3 ;                //Set 3 for half-width, 4 for full-width
-  localparam NUM_LANES             = 2**LOG_NUM_LANES; //Leave untouched
-  localparam NUM_DATA_BYTES        = FPW*16;           //Leave untouched
+  parameter LOG_NUM_LANES         = 3;                //Set 3 for half-width, 4 for full-width
+  parameter NUM_LANES             = 2**LOG_NUM_LANES; //Leave untouched
+  parameter NUM_DATA_BYTES        = FPW*16;           //Leave untouched
   //Define width of the register file
-  localparam HMC_RF_WWIDTH         = 64;   //Leave untouched    
-  localparam HMC_RF_RWIDTH         = 64;   //Leave untouched
-  localparam HMC_RF_AWIDTH         = 4;    //Leave untouched
+  parameter HMC_RF_WWIDTH         = 64;   //Leave untouched    
+  parameter HMC_RF_RWIDTH         = 64;   //Leave untouched
+  parameter HMC_RF_AWIDTH         = 4;    //Leave untouched
   //Configure the Functionality
-  localparam LOG_MAX_RX_TOKENS     = 8;    //Set the depth of the RX input buffer. Must be >= LOG(rf_rx_buffer_rtc) in the RF. Dont't care if OPEN_RSP_MODE=1
-  localparam LOG_MAX_HMC_TOKENS    = 10;   //Set the depth of the HMC input buffer. Must be >= LOG of the corresponding field in the HMC internal register
-  /*
-  localparam HMC_RX_AC_COUPLED     = 1,    //Set to 0 to bypass the run length limiter, saves logic and 1 cycle delay
-  localparam DETECT_LANE_POLARITY  = 1,    //Set to 0 if lane polarity is not applicable, saves logic
-  localparam CTRL_LANE_POLARITY    = 1,    //Set to 0 if lane polarity is not applicable or performed by the transceivers, saves logic and 1 cycle delay
-                                           //If set to 1: Only valid if DETECT_LANE_POLARITY==1, otherwise tied to zero
-  localparam CTRL_LANE_REVERSAL    = 1,    //Set to 0 if lane reversal is not applicable or performed by the transceivers, saves logic
-  localparam CTRL_SCRAMBLERS       = 1,    //Set to 0 to remove the option to disable (de-)scramblers for debugging, saves logic
-  localparam OPEN_RSP_MODE         = 0,    //Set to 1 if running response open loop mode, bypasses the RX input buffer
-  localparam RX_RELAX_INIT_TIMING  = 1,    //Per default, incoming TS1 sequences are only checked for the lane independent h'F0 sequence. Save resources and
+  parameter LOG_MAX_RX_TOKENS     = 10;    //Set the depth of the RX input buffer. Must be >= LOG(rf_rx_buffer_rtc) in the RF. Dont't care if OPEN_RSP_MODE=1
+  parameter LOG_MAX_HMC_TOKENS    = 10;   //Set the depth of the HMC input buffer. Must be >= LOG of the corresponding field in the HMC internal register
+  parameter HMC_RX_AC_COUPLED     = 0;    //Set to 0 to bypass the run length limiter, saves logic and 1 cycle delay
+  parameter DETECT_LANE_POLARITY  = 0;    //Set to 0 if lane polarity is not applicable, saves logic
+  parameter CTRL_LANE_POLARITY    = 0;    //Set to 0 if lane polarity is not applicable or performed by the transceivers, saves logic and 1 cycle delay
+                                          //If set to 1: Only valid if DETECT_LANE_POLARITY==1, otherwise tied to zero
+  parameter CTRL_LANE_REVERSAL    = 0;    //Set to 0 if lane reversal is not applicable or performed by the transceivers, saves logic
+  parameter CTRL_SCRAMBLERS       = 1;    //Set to 0 to remove the option to disable (de-)scramblers for debugging, saves logic
+  parameter OPEN_RSP_MODE         = 0;    //Set to 1 if running response open loop mode, bypasses the RX input buffer
+  parameter RX_RELAX_INIT_TIMING  = 1;    //Per default, incoming TS1 sequences are only checked for the lane independent h'F0 sequence. Save resources and
                                           //eases timing closure. !Lane reversal is still detected
-  localparam RX_BIT_SLIP_CNT_LOG   = 5,    //Define the number of cycles between bit slips. Refer to the transceiver user guide
+  parameter RX_BIT_SLIP_CNT_LOG   = 5;    //Define the number of cycles between bit slips. Refer to the transceiver user guide
                                           //Example: RX_BIT_SLIP_CNT_LOG=5 results in 2^5=32 cycles between two bit slips
-  //localparam SYNC_AXI4_IF          = 0,    //Set to 1 if AXI IF is synchronous to clk_hmc to use simple fifos
-  localparam XIL_CNT_PIPELINED     = 1,    //If Xilinx counters are used, set to 1 to enabled output register pipelining
+  parameter SYNC_AXI4_IF          = 1;    //Set to 1 if AXI IF is synchronous to clk_hmc to use simple fifos
+  parameter XIL_CNT_PIPELINED     = 0;    //If Xilinx counters are used, set to 1 to enabled output register pipelining
   //Set the direction of bitslip. Set to 1 if bitslip performs a shift right, otherwise set to 0 (see the corresponding transceiver user guide)
-  localparam BITSLIP_SHIFT_RIGHT   = 1,    
+  parameter BITSLIP_SHIFT_RIGHT   = 1;    
   //Debug Params
-  localparam DBG_RX_TOKEN_MON      = 1     //Set to 0 to remove the RX Link token monitor, saves logic
-  */
-  localparam FLIT_SIZE = 128 ;
-endclass
+  parameter DBG_RX_TOKEN_MON      = 1;    //Set to 0 to remove the RX Link token monitor, saves logic
 
-typedef rf_agent_cfg #(HMC::HMC_RF_WWIDTH, HMC::HMC_RF_RWIDTH, HMC::HMC_RF_AWIDTH) rf_agent_cfg_t;
-typedef rf_agent     #(HMC::HMC_RF_WWIDTH, HMC::HMC_RF_RWIDTH, HMC::HMC_RF_AWIDTH) rf_agent_t;
+  parameter FLIT_SIZE = 128;
 
-typedef hmc_agent_config #(HMC::DWIDTH, HMC::NUM_LANES, HMC::FPW, HMC::FLIT_SIZE) hmc_agent_config_t;
-typedef hmc_agent        #(HMC::DWIDTH, HMC::NUM_LANES, HMC::FPW, HMC::FLIT_SIZE) hmc_agent_t;
+typedef rf_agent_cfg #(HMC_RF_WWIDTH, HMC_RF_RWIDTH, HMC_RF_AWIDTH) rf_agent_cfg_t;
+typedef rf_agent     #(HMC_RF_WWIDTH, HMC_RF_RWIDTH, HMC_RF_AWIDTH) rf_agent_t;
 
-typedef axi_config #(HMC::NUM_DATA_BYTES, HMC::DWIDTH) axi_config_t;
-typedef axi_agent  #(HMC::NUM_DATA_BYTES, HMC::DWIDTH) axi_agent_t;
+typedef hmc_agent_config #(NUM_LANES) hmc_agent_config_t;
+typedef hmc_agent        #(NUM_LANES) hmc_agent_t;
 
-typedef virtual rf_if #(HMC::HMC_RF_WWIDTH, HMC::HMC_RF_RWIDTH, HMC::HMC_RF_AWIDTH) rf_if_t;
-typedef virtual hmc_agent_if #(HMC::DWIDTH, HMC::NUM_LANES, HMC::FPW, HMC::FLIT_SIZE) hmc_agent_if_t;
-typedef virtual axi_interface #(HMC::NUM_DATA_BYTES, HMC::DWIDTH) axi_interface_t;
+typedef axi_config #(NUM_DATA_BYTES, DWIDTH) axi_config_t;
+typedef axi_agent  #(NUM_DATA_BYTES, DWIDTH) axi_agent_t;
+typedef axi_sequencer  #(NUM_DATA_BYTES, DWIDTH) axi_sequencer_t;
+
+typedef virtual rf_if #(HMC_RF_WWIDTH, HMC_RF_RWIDTH, HMC_RF_AWIDTH) rf_if_t;
+typedef virtual hmc_agent_if #(NUM_LANES) hmc_agent_if_t;
+typedef virtual axi_interface #(NUM_DATA_BYTES, DWIDTH) axi_interface_t;
 
 
 
--- tb/tb_top.sv
+++ tb/tb_top.sv
@@ -1,39 +1,6 @@
-module tb_top;
+`timescale 100ps/1ps
 
-  parameter FPW                   = 4;        //Legal Values: 2,4,6,8
-  parameter LOG_FPW               = 2;        //Legal Values: 1 for FPW=2 ,2 for FPW=4 ,3 for FPW=6/8
-  parameter DWIDTH                = FPW*128;  //Leave untouched
-  //Define HMC interface width
-  parameter LOG_NUM_LANES         = 3;                //Set 3 for half-width, 4 for full-width
-  parameter NUM_LANES             = 2**LOG_NUM_LANES; //Leave untouched
-  parameter NUM_DATA_BYTES        = FPW*16;           //Leave untouched
-  //Define width of the register file
-  parameter HMC_RF_WWIDTH         = 64;   //Leave untouched    
-  parameter HMC_RF_RWIDTH         = 64;   //Leave untouched
-  parameter HMC_RF_AWIDTH         = 4;    //Leave untouched
-  //Configure the Functionality
-  parameter LOG_MAX_RX_TOKENS     = 8;    //Set the depth of the RX input buffer. Must be >= LOG(rf_rx_buffer_rtc) in the RF. Dont't care if OPEN_RSP_MODE=1
-  parameter LOG_MAX_HMC_TOKENS    = 10;   //Set the depth of the HMC input buffer. Must be >= LOG of the corresponding field in the HMC internal register
-  parameter HMC_RX_AC_COUPLED     = 0;    //Set to 0 to bypass the run length limiter, saves logic and 1 cycle delay
-  parameter DETECT_LANE_POLARITY  = 0;    //Set to 0 if lane polarity is not applicable, saves logic
-  parameter CTRL_LANE_POLARITY    = 0;    //Set to 0 if lane polarity is not applicable or performed by the transceivers, saves logic and 1 cycle delay
-                                          //If set to 1: Only valid if DETECT_LANE_POLARITY==1, otherwise tied to zero
-  parameter CTRL_LANE_REVERSAL    = 0;    //Set to 0 if lane reversal is not applicable or performed by the transceivers, saves logic
-  parameter CTRL_SCRAMBLERS       = 1;    //Set to 0 to remove the option to disable (de-)scramblers for debugging, saves logic
-  parameter OPEN_RSP_MODE         = 0;    //Set to 1 if running response open loop mode, bypasses the RX input buffer
-  parameter RX_RELAX_INIT_TIMING  = 1;    //Per default, incoming TS1 sequences are only checked for the lane independent h'F0 sequence. Save resources and
-                                          //eases timing closure. !Lane reversal is still detected
-  parameter RX_BIT_SLIP_CNT_LOG   = 5;    //Define the number of cycles between bit slips. Refer to the transceiver user guide
-                                          //Example: RX_BIT_SLIP_CNT_LOG=5 results in 2^5=32 cycles between two bit slips
-  parameter SYNC_AXI4_IF          = 1;    //Set to 1 if AXI IF is synchronous to clk_hmc to use simple fifos
-  parameter XIL_CNT_PIPELINED     = 0;    //If Xilinx counters are used, set to 1 to enabled output register pipelining
-  //Set the direction of bitslip. Set to 1 if bitslip performs a shift right, otherwise set to 0 (see the corresponding transceiver user guide)
-  parameter BITSLIP_SHIFT_RIGHT   = 1;    
-  //Debug Params
-  parameter DBG_RX_TOKEN_MON      = 1;    //Set to 0 to remove the RX Link token monitor, saves logic
-
-  parameter FLIT_SIZE = 128;
-  parameter CLK_PERIOD = 4.27 ; 
+module tb_top();
 
   `include "uvm_macros.svh"
   import uvm_pkg::*;
@@ -42,24 +9,99 @@ module tb_top;
   import seq_pkg::* ;
   import test_pkg::* ;
 
+  parameter LANE_WIDTH = DWIDTH / NUM_LANES;
+
   logic clk;
+  logic clk_hmc_refclk;
   logic res_n;
 
-  rf_if #(.HMC_RF_WWIDTH(HMC::HMC_RF_WWIDTH),
-          .HMC_RF_RWIDTH(HMC::HMC_RF_RWIDTH),
-          .HMC_RF_AWIDTH(HMC::HMC_RF_AWIDTH))
+//**************** INTERFACES INSTANTIATIONS **************//
+  rf_if #(.HMC_RF_WWIDTH(HMC_RF_WWIDTH),
+          .HMC_RF_RWIDTH(HMC_RF_RWIDTH),
+          .HMC_RF_AWIDTH(HMC_RF_AWIDTH))
     RF (.clk(clk), .res_n(res_n));
 
-  hmc_agent_if #(.DWIDTH(HMC::DWIDTH), 
-                 .NUM_LANES(HMC::NUM_LANES), 
-                 .FPW(HMC::FPW),
-                 .FLIT_SIZE(HMC::FLIT_SIZE))
-  HMC_IF (.clk(clk), .res_n(res_n));
+  hmc_agent_if #(.NUM_LANES(NUM_LANES)) 
+  HMC_IF ();
 
- axi_interface #(.DWIDTH(HMC::DWIDTH),
-                 .NUM_DATA_BYTES(HMC::NUM_DATA_BYTES))
+  axi_interface #(.DWIDTH(DWIDTH),
+                 .NUM_DATA_BYTES(NUM_DATA_BYTES))
   AXI_IF (.clk(clk), .res_n(res_n));
 
+//*******************************************************//
+
+//*************** Handling HMC interface ***************//
+
+//----------------------------- Wiring openHMC controller
+wire [DWIDTH-1:0]       to_serializers;
+wire [DWIDTH-1:0]       from_deserializers;
+wire [NUM_LANES-1:0]    bit_slip;
+wire [NUM_LANES-1:0]    phy_lane_polarity;
+bit                     phy_rx_ready;
+bit                     P_RST_N;
+// Wire the HMC interface in the openHMC
+wire            LxRXPS; // HMC input
+wire            LxTXPS; // HMC output
+wire            FERR_N; // HMC output
+wire            LxTXPS_pullup;
+assign          LxTXPS_pullup = (LxTXPS === 1'bz) ? 1'b1 : LxTXPS;
+wire            FERR_N_pullup;
+assign          FERR_N_pullup = (FERR_N === 1'bz) ? 1'b1 : FERR_N;
+//------------------------------ Attach the HMC Link interface
+ assign HMC_IF.REFCLKP = clk_hmc_refclk;
+ assign HMC_IF.REFCLKN = ~clk_hmc_refclk;
+ assign FERR_N  = HMC_IF.FERR_N;
+ assign HMC_IF.REFCLK_BOOT = 2'b00; // 00 -> 125 MHz, 01 -> 156.25 MHz, 10 -> 166.67 MHz
+ assign HMC_IF.P_RST_N = P_RST_N;
+ assign LxTXPS = HMC_IF.TXPS;
+ assign HMC_IF.RXPS = LxRXPS;
+  
+ assign HMC_IF.RXP = NUM_LANES==8 ? {8'h0, serial_Txp[NUM_LANES-1:0]} : serial_Txp; // Controller Tx is Cube Rx
+ assign HMC_IF.RXN = ~HMC_IF.RXP;//NUM_LANES==8 ? {8'h0, ~serial_Txp[NUM_LANES-1:0]} : ~serial_Txp; // Controller Tx is Cube Rx
+ assign serial_Rx = HMC_IF.TXP; // Controller Rx is Cube Tx
+
+//----------------------------- Generate a fast clock for serializers
+bit clk_10G;
+generate
+    begin : clocking_gen
+        initial clk_10G = 1'b1;
+        always #0.05ns clk_10G = ~clk_10G;  
+    end
+endgenerate
+
+bit LxTXPS_synced;
+genvar lane;
+generate
+  begin : serializers_gen
+      for (lane=0; lane<NUM_LANES; lane++) begin : behavioral_gen
+          serializer #(
+              .DWIDTH(LANE_WIDTH)
+          ) serializer_I (
+              .clk(clk),
+              .res_n(res_n),
+              .fast_clk(clk_10G),
+              .data_in(to_serializers[lane*LANE_WIDTH+LANE_WIDTH-1:lane*LANE_WIDTH]),
+              .data_out(serial_Txp[lane])
+          );
+          deserializer #(
+              .DWIDTH(LANE_WIDTH)
+          ) deserializer_I (
+              .clk(clk),
+              .res_n(LxTXPS_synced && res_n),
+              .fast_clk(clk_10G),
+              .bit_slip(bit_slip[lane]),
+              .lane_polarity(phy_lane_polarity[lane]),
+              .data_in(serial_Rx[lane]),
+              .data_out(from_deserializers[lane*LANE_WIDTH+LANE_WIDTH-1:lane*LANE_WIDTH])
+          );
+      end
+  end
+endgenerate
+always @(LxTXPS) phy_rx_ready   <= #500ns LxTXPS;
+always @(posedge clk) LxTXPS_synced <= LxTXPS;
+//*******************************************************//
+
+//***************** DUT INSTANTIATION *****************//
   openhmc_top #(.FPW(FPW),
                 .LOG_FPW(LOG_FPW),
                 .DWIDTH(DWIDTH),
@@ -96,19 +138,19 @@ module tb_top;
          .m_axis_rx_TREADY(AXI_IF.rx_ready),
          .m_axis_rx_TDATA(AXI_IF.rx_data),
          .m_axis_rx_TUSER(AXI_IF.rx_user),
-         // transceiver
-         .phy_data_tx_link2phy(HMC_IF.phy_data_tx_link2phy),
-         .phy_data_rx_phy2link(HMC_IF.phy_data_rx_phy2link),
-         .phy_bit_slip(HMC_IF.phy_bit_slip), //Must be connected if DETECT_LANE_POLARITY==1 AND CTRL_LANE_POLARITY=0
-         .phy_lane_polarity(HMC_IF.phy_lane_polarity), //All 0 if CTRL_LANE_POLARITY=1
-         .phy_tx_ready(HMC_IF.phy_tx_ready), //Optional information to RF
-         .phy_rx_ready(HMC_IF.phy_rx_ready), //Release RX descrambler reset when PHY ready
-         .phy_init_cont_set(HMC_IF.phy_init_cont_set), //Can be used to release transceiver reset if used
+         // transceiver (physical link)
+         .phy_data_tx_link2phy(to_serializers),
+         .phy_data_rx_phy2link(from_deserializers),
+         .phy_bit_slip(bit_slip), //Must be connected if DETECT_LANE_POLARITY==1 AND CTRL_LANE_POLARITY=0
+         .phy_lane_polarity(phy_lane_polarity), //All 0 if CTRL_LANE_POLARITY=1
+         .phy_tx_ready(res_n), //Optional information to RF
+         .phy_rx_ready(phy_rx_ready && LxTXPS), //Release RX descrambler reset when PHY ready
+         .phy_init_cont_set(), //Can be used to release transceiver reset if used
          // hmc
-         .P_RST_N(HMC_IF.P_RST_N),
-         .LXRXPS(HMC_IF.LXRXPS),
-         .LXTXPS(HMC_IF.LXTXPS),
-         .FERR_N(HMC_IF.FERR_N),
+         .P_RST_N(P_RST_N),
+         .LXRXPS(LXRXPS),
+         .LXTXPS(LxTXPS_pullup),
+         .FERR_N(FERR_N_pullup),
          // register file
          .rf_address(RF.rf_address),
          .rf_read_data(RF.rf_read_data),
@@ -118,7 +160,9 @@ module tb_top;
          .rf_write_en(RF.rf_write_enable),
          .rf_write_data(RF.rf_write_data)
          );
+//*****************************************************//
 
+//********************** ASSERTIONS MODULE ***********************//
  bind openhmc_top : dut openhmc_sva #(.FPW(FPW),
                                        .LOG_FPW(LOG_FPW),
                                        .DWIDTH(DWIDTH),
@@ -142,29 +186,48 @@ module tb_top;
                                        .BITSLIP_SHIFT_RIGHT(BITSLIP_SHIFT_RIGHT),
                                        .DBG_RX_TOKEN_MON(DBG_RX_TOKEN_MON)) 
                          openhmc_sva_1 (.*);
-                         
+//****************************************************************//                       
 
 initial begin
-  uvm_config_db#(virtual rf_if #(HMC::HMC_RF_WWIDTH, HMC::HMC_RF_RWIDTH, HMC::HMC_RF_AWIDTH))::set(null, "uvm_test_top", "RF", RF);
-  uvm_config_db#(virtual hmc_agent_if #(HMC::DWIDTH, HMC::NUM_LANES, HMC::FPW, HMC::FLIT_SIZE))::set(null, "uvm_test_top", "HMC_IF", HMC_IF);
-  uvm_config_db#(virtual axi_interface #(HMC::NUM_DATA_BYTES, HMC::DWIDTH))::set(null, "uvm_test_top", "AXI_IF", AXI_IF);
+  uvm_config_db#(virtual rf_if #(HMC_RF_WWIDTH, HMC_RF_RWIDTH, HMC_RF_AWIDTH))::set(null, "uvm_test_top", "RF", RF);
+  uvm_config_db#(virtual hmc_agent_if #(NUM_LANES))::set(null, "uvm_test_top", "HMC_IF", HMC_IF);
+  uvm_config_db#(virtual axi_interface #(NUM_DATA_BYTES, DWIDTH))::set(null, "uvm_test_top", "AXI_IF", AXI_IF);
   run_test();
 end
 
-initial begin
-  res_n = 0;
-  clk = 0;
-  repeat(10) begin
-    //#10ns
-    #(CLK_PERIOD/2)
-     clk = ~clk;
+//*********** CLOCK **********//
+  always begin
+    case(FPW)
+        2: begin
+            if(LOG_NUM_LANES==3) //8 or 16 lanes
+                #1.6ns clk = !clk;
+            else begin
+              #0.8ns clk = !clk;
+            end
+        end
+        4: begin
+            if(LOG_NUM_LANES==3)
+                #3.2ns clk = !clk;
+            else
+                #1.6ns clk = !clk;
+        end
+        6: begin
+            if(LOG_NUM_LANES==3)
+                #4.8ns clk = !clk;
+            else
+                #2.4ns clk = !clk;
+        end
+        8: begin
+            if(LOG_NUM_LANES==3)
+                #6.4ns clk = !clk;
+            else
+                #3.2ns clk = !clk;
+        end
+    endcase
   end
-  res_n = 1;
-  forever begin
-    //#10ns 
-    #(CLK_PERIOD/2)
-    clk = ~clk;
-  end
-end
+  //-- 125 MHz HMC/Transceiver refclock
+  always #4ns clk_hmc_refclk <= ~clk_hmc_refclk;
+//****************************//
+
 
 endmodule : tb_top
